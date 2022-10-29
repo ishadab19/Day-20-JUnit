4f8954e7831c19a3d3cc7f2315fdb4eb8252f81b
shadab <me.shadab19@gmail.com>
2022-10-29 23:18:14 +0530

    [Added - Testing of Method Email]

--- src/com/JUnit/java/userRegistration.java
+++ src/com/JUnit/java/userRegistration.java
@@ -6,7 +6,6 @@ import java.util.regex.Pattern;
 import org.junit.Test;
 
 public class userRegistration {
-	
 
 	@Test
 	public boolean firstName(String firstName) {
@@ -15,11 +14,20 @@ public class userRegistration {
 		Matcher matcher = pattern.matcher(firstName);
 		return matcher.matches();
 	}
+
 	@Test
 	public boolean lastName(String lastname) {
 		String regex = "^[A-Z]{1}[a-z]{2,}$";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(lastname);
+		return matcher.matches();
+	}
+
+	@Test
+	public boolean email(String email) {
+		String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(email);
 		return matcher.matches();
 	}
 }
--- src/com/JUnit/java/userRegistrationTest.java
+++ src/com/JUnit/java/userRegistrationTest.java
@@ -5,35 +5,48 @@ import org.junit.Test;
 import junit.framework.Assert;
 
 public class userRegistrationTest {
-	 userRegistration userRegistration = new userRegistration();
-	 
+	userRegistration userRegistration = new userRegistration();
 
-	    @Test
-	    public void givenFirstName_WhenProper_ShouldReturnTrue() {
-	        boolean result = userRegistration.firstName("Shadab");
-	        Assert.assertEquals(true, result);
-	    }
-	    @Test
-	    public void givenFirstName_whenNotProper_ShouldReturnFalse() {
-	    	boolean result = userRegistration.firstName("shadab");
-	    	Assert.assertEquals(false, result);
-	    }
-	    @Test
-	    public void givenLastName_whenProper_ShouldReturnTrue() {
-	    	boolean result = userRegistration.lastName("Hussain");
-	    	Assert.assertEquals(true, result);
-	    }
-	    @Test
-	    public void givenLastName_WhenNotProper_ShouldReturnFalse() {
-	    	boolean result = userRegistration.lastName("hussain");
-	    	Assert.assertEquals(false, result);
-	    }
-	
-	    @Test
-    	    public void givenFirstName_WhenNotProper_ShouldReturnFalse() {
-            boolean result = userRegistration.firstName("shadab");
-            Assert.assertEquals(false, result);
-	    
-	    }
+	@Test
+	public void givenFirstName_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.firstName("Shadab");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenFirstName_whenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.firstName("shadab");
+		Assert.assertEquals(false, result);
+	}
+
+	@Test
+	public void givenLastName_whenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.lastName("Hussain");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenLastName_WhenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.lastName("hussain");
+		Assert.assertEquals(false, result);
+	}
+
+	@Test
+	public void givenFirstName_WhenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.firstName("shadab");
+		Assert.assertEquals(false, result);
+
+	}
+
+	@Test
+	public void givenEmail_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.email("shadab@gmail.com");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenEmail_WhenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.email("shadab&.com");
+		Assert.assertEquals(false, result);
+	}
 }
-
