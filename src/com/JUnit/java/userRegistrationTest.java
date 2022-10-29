dc662c108c16cd1a538d1094003a39d45924a11b
shadab <me.shadab19@gmail.com>
2022-10-29 22:34:53 +0530

    [Added - Testing of Method FirstName]

---
+++ src/com/JUnit/java/userRegistrationTest.java
@@ -0,0 +1,17 @@
+package com.JUnit.java;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class userRegistrationTest {
+	 userRegistration userRegistration = new userRegistration();
+
+	    @Test
+	    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+	        boolean result = userRegistration.firstName("Shadab");
+	        Assert.assertEquals(true, result);
+	    }
+	
+}
+
